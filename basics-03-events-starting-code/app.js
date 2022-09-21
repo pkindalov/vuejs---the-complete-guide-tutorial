c95cec90ea30c4ca9ab88b77013b27a00a353d47
pkindalov <panayotkindalov@gmail.com>
2022-09-21 15:58:22 +0300

    Updated example

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,10 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      // event.preventDefault();
+      alert('Submitted');
+    },
     setName(event, family) {
       this.name = event.target.value + ' ' + family;
     },
