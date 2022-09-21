dfb5a79d9f52f778b57ea3c2547445a6495f7ec6
pkindalov <panayotkindalov@gmail.com>
2022-09-21 11:36:18 +0300

    Extracted logic to functions

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add() {
+      this.counter++;
+    },
+    subtract() {
+      this.counter--;
+    }
+  }
 });
 
 app.mount('#events');
