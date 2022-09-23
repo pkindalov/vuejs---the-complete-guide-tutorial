d76ffaad5b17f54fe457ba8a156f21a3888d7ba0
pkindalov <panayotkindalov@gmail.com>
2022-09-23 11:57:46 +0300

    Added inline styles dynamically example

---
+++ section 2/basics-10-styling-starting-setup/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false
+        }
+    },
+    methods: {
+        boxSelected(box) {
+            if (box === 'A') this.boxASelected = true;
+            if (box === 'B') this.boxBSelected = true;
+            if (box === 'C') this.boxCSelected = true;
+        }
+    }
+});
+
+app.mount('#styling');
