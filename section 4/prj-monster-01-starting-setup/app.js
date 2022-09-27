bfe6f706717d1801a9d01b9b3e88d1b4109c102b
pkindalov <panayotkindalov@gmail.com>
2022-09-27 22:20:06 +0300

    Updated health bars, added a special attack and other small changes

--- section 4/prj-monster-01-starting-setup/app.js
+++ section 4/prj-monster-01-starting-setup/app.js
@@ -6,11 +6,24 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      currentRound: 0
+    }
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + '%' };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + '%' }
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
     }
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRendomValue(5, 12);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
@@ -18,8 +31,14 @@ const app = Vue.createApp({
     attackPlayer() {
       const attackValue = getRendomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRendomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     }
   }
 });
 
-app.mounth('#game');
+app.mount('#game');
