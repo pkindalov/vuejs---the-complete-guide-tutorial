3748b916e9721593ca22ab3c4484d2445a12d9b1
pkindalov <panayotkindalov@gmail.com>
2022-10-12 14:13:17 +0300

    Added methods for starting a new game and surrending. Other small changes. Formatting with 4 spaces

--- section 4/prj-monster-01-starting-setup/app.js
+++ section 4/prj-monster-01-starting-setup/app.js
@@ -1,76 +1,87 @@
 function getRendomValue(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
-  data() {
-    return {
-      playerHealth: 100,
-      monsterHealth: 100,
-      currentRound: 0,
-      winner: null
-    }
-  },
-  watch: {
-    playerHealth(value) {
-      if (value <= 0 && this.monsterHealth <= 0) {
-        //draw
-        this.winner = 'draw';
-      } else if (value <= 0) {
-        //player lost
-        this.winner = 'monster';
-      }
+    data() {
+        return {
+            playerHealth: 100,
+            monsterHealth: 100,
+            currentRound: 0,
+            winner: null
+        }
     },
-    monsterHealth(value) {
-      if (value <= 0 && this.playerHealth <= 0) {
-        //draw
-        this.winner = 'draw';
-      } else if (value <= 0) {
-        //monster lost
-        this.winner = 'player';
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                //draw
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                //player lost
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                //draw
+                this.winner = 'draw';
+            } else if (value <= 0) {
+                //monster lost
+                this.winner = 'player';
 
-      }
+            }
+        }
+    },
+    computed: {
+        monsterBarStyles() {
+            if (this.monsterHealth < 0) return { width: '0%' };
+            return { width: this.monsterHealth + '%' };
+        },
+        playerBarStyles() {
+            if (this.playerHealth < 0) return { width: '0%' };
+            return { width: this.playerHealth + '%' }
+        },
+        mayUseSpecialAttack() {
+            return this.currentRound % 3 !== 0;
+        }
+    },
+    methods: {
+        startGame() {
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+            this.winner = null;
+        },
+        attackMonster() {
+            this.currentRound++;
+            const attackValue = getRendomValue(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getRendomValue(8, 15);
+            this.playerHealth -= attackValue;
+        },
+        specialAttackMonster() {
+            this.currentRound++;
+            const attackValue = getRendomValue(10, 25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRendomValue(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100;
+                return;
+            }
+            this.playerHealth += healValue;
+            this.attackPlayer();
+        },
+        surrender() {
+            this.winner = 'monster';
+        }
     }
-  },
-  computed: {
-    monsterBarStyles() {
-      return { width: this.monsterHealth + '%' };
-    },
-    playerBarStyles() {
-      return { width: this.playerHealth + '%' }
-    },
-    mayUseSpecialAttack() {
-      return this.currentRound % 3 !== 0;
-    }
-  },
-  methods: {
-    attackMonster() {
-      this.currentRound++;
-      const attackValue = getRendomValue(5, 12);
-      this.monsterHealth -= attackValue;
-      this.attackPlayer();
-    },
-    attackPlayer() {
-      const attackValue = getRendomValue(8, 15);
-      this.playerHealth -= attackValue;
-    },
-    specialAttackMonster() {
-      this.currentRound++;
-      const attackValue = getRendomValue(10, 25);
-      this.monsterHealth -= attackValue;
-      this.attackPlayer();
-    },
-    healPlayer() {
-      this.currentRound++;
-      const healValue = getRendomValue(8, 20);
-      if (this.playerHealth + healValue > 100) {
-        this.playerHealth = 100;
-        return;
-      }
-      this.playerHealth += healValue;
-      this.attackPlayer();
-    }
-  }
 });
 
 app.mount('#game');
