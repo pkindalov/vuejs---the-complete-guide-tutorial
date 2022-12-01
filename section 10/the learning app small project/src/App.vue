2efe0735c4a9eb49572a78913a8a04933538cb2a
pkindalov <panayotkindalov@gmail.com>
2022-12-01 20:13:41 +0200

    Added section 10

---
+++ section 10/the learning app small project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js documentation',
+          link: 'https://www.vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
