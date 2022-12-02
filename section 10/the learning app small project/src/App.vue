5bbf82ee9b6fa547bc1c64fe3de77e02fb133db9
pkindalov <panayotkindalov@gmail.com>
2022-12-02 11:59:51 +0200

    Added new components - base-card which is registered globally and the-header registered locally and used them.

--- section 10/the learning app small project/src/App.vue
+++ section 10/the learning app small project/src/App.vue
@@ -1,12 +1,15 @@
 <template>
+  <the-header title="Remember Me"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredRecources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 export default {
   components: {
     StoredResources,
+    TheHeader,
   },
   data() {
     return {
