eccffd374af302969737b5f5f94708ed4c0f3acc
pkindalov <panayotkindalov@gmail.com>
2022-12-02 20:44:35 +0200

    Added new components the-resources. and used it in app component and AddResource. Moved stored-resource component from app to the-resource component. Moved storedResources data from app to the-resource component and provide it from there to the stored-resource component. Used new component add-resource in the-resource component and also created and used a new dynamic component.

--- section 10/the learning app small project/src/App.vue
+++ section 10/the learning app small project/src/App.vue
@@ -1,33 +1,15 @@
 <template>
   <the-header title="Remember Me"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <the-resources></the-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredRecources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
 export default {
   components: {
-    StoredResources,
     TheHeader,
-  },
-  data() {
-    return {
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The Official Vue.js documentation',
-          link: 'https://www.vuejs.org',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to google...',
-          link: 'https://google.org',
-        },
-      ],
-    };
+    TheResources,
   },
 };
 </script>
