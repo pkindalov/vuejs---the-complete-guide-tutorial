d322c463c918cbf44a0838db6d1e81e46fd8ed4a
pkindalov <panayotkindalov@gmail.com>
2022-12-05 16:38:02 +0200

    Added new component base-dialog for custom alert messages and register it like a global component

--- section 10/the learning app small project/src/components/learning-resources/AddResource.vue
+++ section 10/the learning app small project/src/components/learning-resources/AddResource.vue
@@ -1,5 +1,20 @@
 <template>
-  <!-- <h2>Add Resource</h2> -->
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid Input"
+    @close="confirmError"
+  >
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid</p>
+      <p>
+        Please check all inputs and make sure you enter at least a few
+        characters into each input field.
+      </p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Ok</base-button>
+    </template>
+  </base-dialog>
   <base-card>
     <form action="" method="post" @submit.prevent="submitData">
       <div class="form-control">
@@ -16,7 +31,7 @@
         ></textarea>
       </div>
       <div class="form-control">
-        <label for="link">Title</label>
+        <label for="link">Link</label>
         <input type="url" name="link" id="link" ref="linkInput" />
       </div>
       <div>
@@ -29,13 +44,30 @@
 <script>
 export default {
   inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
   methods: {
     submitData() {
       const enteredTitle = this.$refs.titleInput.value;
       const enteredDesc = this.$refs.descInput.value;
       const enteredLink = this.$refs.linkInput.value;
 
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDesc.trim() === '' ||
+        enteredLink.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
       this.addResource(enteredTitle, enteredDesc, enteredLink);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
     },
   },
 };
