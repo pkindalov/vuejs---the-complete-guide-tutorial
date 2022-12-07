ff429b1f7479c8edd0a6b15d3ee0786b1fb11c33
pkindalov <panayotkindalov@gmail.com>
2022-12-07 15:26:33 +0200

    Handling error responses

--- section 12/http-01-starting-setup/src/components/survey/LearningSurvey.vue
+++ section 12/http-01-starting-setup/src/components/survey/LearningSurvey.vue
@@ -41,6 +41,7 @@
         <p v-if="invalidInput">
           One or more input fields are invalid. Please check your provided data.
         </p>
+        <p v-if="error">{{ error }}</p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -56,6 +57,7 @@ export default {
       enteredName: '',
       chosenRating: null,
       invalidInput: false,
+      error: null,
     };
   },
   // emits: ['survey-submit'],
@@ -72,6 +74,7 @@ export default {
       //   rating: this.chosenRating,
       // });
 
+      this.error = null;
       fetch(
         'https://vue-http-demo-165cc-default-rtdb.europe-west1.firebasedatabase.app/surveys.json',
         {
@@ -84,7 +87,18 @@ export default {
             rating: this.chosenRating,
           }),
         }
-      );
+      )
+        .then((response) => {
+          if (response.ok) {
+            //...
+          } else {
+            throw new Error('Could not save data!');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+          this.error = error.message;
+        });
 
       this.enteredName = '';
       this.chosenRating = null;
