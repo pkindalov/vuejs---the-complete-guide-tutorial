51a656cc6d01406231fbda0c4c935cf875bbc046
pkindalov <panayotkindalov@gmail.com>
2022-12-08 13:13:02 +0200

    Redirecting &  'Catch' all routes. Added new simple component - NotFound

--- section 13/routing-01-starting-setup/src/main.js
+++ section 13/routing-01-starting-setup/src/main.js
@@ -3,15 +3,20 @@ import { createRouter, createWebHistory } from 'vue-router';
 import TeamList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue'
+import NotFound from './components/nav/NotFound.vue';
 
 import App from './App.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamList },
+    // { path: '/teams', component: TeamList, alias: '/' },
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true }
+    { path: '/teams/:teamId', component: TeamMembers, props: true },
+    // { path: '/:notFound(.*)', redirect: '/teams' }
+    { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
 });
