4b730e26c4b7cd766ae02afbaa1481ee9049e2a4
pkindalov <panayotkindalov@gmail.com>
2022-12-12 09:55:36 +0200

    Beyond entering: route leaves guards. Using beforeRouteLeave guard in small example

--- section 13/routing-01-starting-setup/src/components/users/UsersList.vue
+++ section 13/routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
   <button @click="confirmInput">Confirm</button>
+  <button @click="saveChanges">Save Changes</button>
   <ul>
     <user-item
       v-for="user in users"
@@ -18,16 +19,37 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
   methods: {
     confirmInput() {
       //do something.. this is used just for the example about programmatic navigation
       this.$router.push('/');
+    },
+    saveChanges() {
+      this.changesSaved = true;
     },
   },
   beforeRouteEnter(to, from, next) {
     console.log('UsersList component beforeRouteEnter');
     console.log(to, from);
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('UsersList component beforeRouteLeave: ');
+    console.log(to, from);
+    if (this.changesSaved) {
+      next();
+      return;
+    }
+    const userWantsToLeave = confirm('Are you sure? You have unsaved changes');
+    next(userWantsToLeave);
+  },
+  unmounted() {
+    console.log('Unmounted');
   },
 };
 </script>
