882d32ed4c1b0f31d43057860bb9cc6019b3d6d2
pkindalov <panayotkindalov@gmail.com>
2022-12-13 15:19:04 +0200

    Building Javascript transitions(instead of css)

--- section 14/animations-01-starting-setup/src/App.vue
+++ section 14/animations-01-starting-setup/src/App.vue
@@ -24,6 +24,8 @@
       @before-leave="beforeLeave"
       @leave="leave"
       @after-leave="afterLeave"
+      @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled"
     >
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
@@ -54,16 +56,38 @@ export default {
       dialogIsVisible: false,
       paraIsVisible: false,
       usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
     };
   },
   methods: {
+    enterCancelled(el) {
+      console.log('enterCancelled');
+      console.log(el);
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled(el) {
+      console.log('leaveCancelled');
+      console.log(el);
+      clearInterval(this.leaveInterval);
+    },
     beforeEnter(el) {
       console.log('beforeEnter');
       console.log(el);
+      el.style.opacity = 0;
     },
-    enter(el) {
+    enter(el, done) {
       console.log('enter');
       console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
     },
     afterEnter(el) {
       //after animation is finished
@@ -73,10 +97,32 @@ export default {
     beforeLeave(el) {
       console.log('beforeLeave');
       console.log(el);
+      el.style.opacity = 1;
     },
-    leave(el) {
+    leave(el, done) {
       console.log('leave');
       console.log(el);
+      //author way
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+
+      //my way
+      // let round = 100;
+      // const interval = setInterval(function () {
+      //   el.style.opacity = round * 0.01;
+      //   round--;
+      //   if (round === 0) {
+      //     clearInterval(interval);
+      //     done();
+      //   }
+      // }, 20);
     },
     afterLeave(el) {
       console.log('afterLeave');
@@ -167,44 +213,6 @@ button:active {
 .animate {
   /* transform: translateX(-150px); */
   animation: slide-scale 0.3s ease-out forwards;
-}
-
-/* We should include at least one transition or animation, 
-so Vue to be able to read the duration time from these classes */
-
-/* when the element is appearing in the dom for the first time */
-.para-enter-from {
-  /* opacity: 0;
-  transform: translateY(-30px); */
-}
-
-/* this is the place where for example add the transition css property 
-   to tell Vue to watch for all css properties that might be animated 
-*/
-.para-enter-active {
-  /* transition: all 0.3s ease-out; */
-  animation: slide-scale 2s ease-out;
-}
-
-/* End or final state */
-.para-enter-to {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
-
-.para-leave-from {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
-
-.para-leave-active {
-  /* transition: all 0.3s ease-in; */
-  animation: slide-scale 0.3s ease-out;
-}
-
-.para-leave-to {
-  /* opacity: 0;
-  transform: translateY(30px); */
 }
 
 @keyframes slide-scale {
