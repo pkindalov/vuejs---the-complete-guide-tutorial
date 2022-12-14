b531677d112d2c901d1443996b8714001c62c7f5
pkindalov <panayotkindalov@gmail.com>
2022-12-14 11:01:55 +0200

    Animating list withs with transition-group

--- section 14/animations-01-starting-setup/src/components/UsersList.vue
+++ section 14/animations-01-starting-setup/src/components/UsersList.vue
@@ -1,9 +1,20 @@
 <template>
-  <ul>
+  <!-- transition element don't render any element in dom 
+       transition-group however put an element into dom. You can choose what element
+       to add using the tag attribute on transition-group element and put the name of
+       the tag inside. You can use a valid html tags in there and also to use a custom component
+
+       transition-group works pretty much like the transition element. It also add these css classes
+       (v-enter-to, v-enter-from, v-enter-active, v-leave-to, v-leave-from, v-leave-active etc..)
+       to the element which are added or removed. It dives into the css code to see how long the
+       animation takes.
+  -->
+  <transition-group tag="ul" name="user-list">
     <li v-for="user in users" :key="user" @click="removeUser(user)">
       {{ user }}
     </li>
-  </ul>
+  </transition-group>
+
   <div>
     <input type="text" ref="userNameInput" />
     <button @click="addUser">Add User</button>
@@ -40,5 +51,26 @@ li {
   border: 1px solid #ccc;
   padding: 1rem;
   text-align: center;
+}
+
+.user-list-enter-from {
+  opacity: 0;
+  transform: translateX(-30px);
+}
+.user-list-enter-active {
+  transition: all 1s ease-out;
+}
+.user-list-enter-to,
+.user-list-leave-from {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+.user-list-leave-active {
+  transition: all 1s ease-in;
+}
+.user-list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
