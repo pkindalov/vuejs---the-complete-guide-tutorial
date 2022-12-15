5fda91debe49d00d3ab941a05c28136497939b2f
pkindalov <panayotkindalov@gmail.com>
2022-12-15 10:26:28 +0200

    Introducing mutations  - a better way of changing data. Added a small note

--- section 15/vuex-01-starting-setup/src/main.js
+++ section 15/vuex-01-starting-setup/src/main.js
@@ -8,6 +8,18 @@ const store = createStore({
     return {
       counter: 0
     }
+  },
+  //Mutations are clearly defined methods, which have the logic to update the state.
+  //And from inside our components, we should, in the end, just trigger those mutations
+  //instead of directly manipulating the state.
+
+  //By triggering mutations, all components that want to edit a state do it
+  //in the same way. And it's guaranteed that they do it in the same way.
+
+  mutations: {
+    increment(state) {
+      state.counter = state.counter + 2;
+    }
   }
 });
 const app = createApp(App);
