ad287ea38f1cea8e6a83be08bf3699fbe70a0616
pkindalov <panayotkindalov@gmail.com>
2022-12-16 11:50:32 +0200

    Organizing your store with Modules

--- section 15/vuex-01-starting-setup/src/main.js
+++ section 15/vuex-01-starting-setup/src/main.js
@@ -3,10 +3,50 @@ import { createStore } from 'vuex';
 
 import App from './App.vue';
 
-const store = createStore({
+const counterModule = {
   state() {
     return {
       counter: 0,
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.counter = state.counter + 2;
+    },
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    },
+  },
+  actions: {
+    increment(context) {
+      setTimeout(function () {
+        context.commit('increment');
+      }, 2000);
+    },
+    increase(context, payload) {
+      context.commit('increase', payload);
+    },
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 3;
+    },
+    normalizedCounter(state, getters) {
+      const finalCounter = getters.finalCounter;
+      if (finalCounter < 0) return 0;
+      if (finalCounter > 100) return 100;
+      return finalCounter;
+    },
+  }
+};
+
+const store = createStore({
+  modules: {
+    numbers: counterModule
+  },
+  state() {
+    return {
+
       // isLogged: false //my version
       isLoggedIn: false, //author version
     }
@@ -19,12 +59,6 @@ const store = createStore({
   //in the same way. And it's guaranteed that they do it in the same way.
 
   mutations: {
-    increment(state) {
-      state.counter = state.counter + 2;
-    },
-    increase(state, payload) {
-      state.counter = state.counter + payload.value;
-    },
     //my version
     // logIn(state) {
     //   state.isLogged = true;
@@ -40,14 +74,6 @@ const store = createStore({
     }
   },
   actions: {
-    increment(context) {
-      setTimeout(function () {
-        context.commit('increment');
-      }, 2000);
-    },
-    increase(context, payload) {
-      context.commit('increase', payload);
-    },
     //my version  
     // logIn(context) {
     //   context.commit('logIn');
@@ -63,15 +89,6 @@ const store = createStore({
     }
   },
   getters: {
-    finalCounter(state) {
-      return state.counter * 3;
-    },
-    normalizedCounter(state, getters) {
-      const finalCounter = getters.finalCounter;
-      if (finalCounter < 0) return 0;
-      if (finalCounter > 100) return 100;
-      return finalCounter;
-    },
     // my version
     // isLogged(state) {
     //   return state.isLogged;
