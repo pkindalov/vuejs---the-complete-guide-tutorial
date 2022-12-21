e90022c2905b2770d08823bc50d09c2e594c31f2
pkindalov <panayotkindalov@gmail.com>
2022-12-21 14:00:03 +0200

    Added store for module products initialized the state of store and loaded it as a module in the main store

--- section 15/vuex-11 - exercise - my version/src/store/index.js
+++ section 15/vuex-11 - exercise - my version/src/store/index.js
@@ -3,11 +3,12 @@ import rootMutations from './mutations.js';
 import rootActions from './actions.js';
 import rootGetters from './getters.js';
 import cartModule from './modules/cart/index.js';
+import productModule from './modules/products/index.js'
 
 const store = createStore({
   modules: {
     cart: cartModule,
-    // product: productModule
+    product: productModule
   },
   state() {
     return {
