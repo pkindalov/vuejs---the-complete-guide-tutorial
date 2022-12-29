be9576eba4eae36bae61d03e997ae8f5106b77af
pkindalov <panayotkindalov@gmail.com>
2022-12-29 16:28:03 +0200

    Registering as coach - the form

--- section 16/main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
+++ section 16/main-prj-01-starting-setup/src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,19 @@
-<template>REGISTER</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now !</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+import BaseCard from '../../components/ui/BaseCard.vue';
+export default {
+  components: {
+    CoachForm,
+    BaseCard,
+  },
+};
+</script>
