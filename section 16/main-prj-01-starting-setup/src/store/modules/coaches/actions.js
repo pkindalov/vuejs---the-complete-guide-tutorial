45b1a11af3d0282ade82d063b66ba5d83ddb7119
pkindalov <panayotkindalov@gmail.com>
2023-01-02 15:13:09 +0200

    Fetching Coach Data (GET Http Request)

--- section 16/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ section 16/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -27,4 +27,29 @@ export default {
       id: userId,
     });
   },
+  async loadCoaches(contex) {
+    const response = await fetch(
+      'https://vue-http-demo-165cc-default-rtdb.europe-west1.firebasedatabase.app/coaches.json'
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      //error handling later
+    }
+
+    const coaches = [];
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+
+      coaches.push(coach);
+    }
+    contex.commit('setCoaches', coaches);
+  },
 };
