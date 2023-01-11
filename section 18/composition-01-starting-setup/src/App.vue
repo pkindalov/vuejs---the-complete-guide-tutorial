a294fb7cb25ec6c8c6555e332cd96a9084fed7a3
pkindalov <panayotkindalov@gmail.com>
2023-01-11 12:19:05 +0200

    Replacing "data" with "refs"

--- section 18/composition-01-starting-setup/src/App.vue
+++ section 18/composition-01-starting-setup/src/App.vue
@@ -5,12 +5,22 @@
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
-  data() {
-    return {
-      userName: 'Maximilian',
-    };
+  setup() {
+    const uName = ref('Maximilian');
+
+    setTimeout(function () {
+      uName.value = 'Max';
+    }, 2000);
+
+    return { userName: uName };
   },
+  // data() {
+  //   return {
+  //     userName: 'Maximilian',
+  //   };
+  // },
 };
 </script>
 
