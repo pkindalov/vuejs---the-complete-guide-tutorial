638506ff5b4945bdaa9f814d90658178c31beea7
pkindalov <panayotkindalov@gmail.com>
2023-01-11 14:31:48 +0200

    Building "reactive" Objects

--- section 18/composition-01-starting-setup/src/App.vue
+++ section 18/composition-01-starting-setup/src/App.vue
@@ -1,20 +1,43 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
+// import { ref } from 'vue';
+
+//for objects - reactive works only with objects
+import { reactive } from 'vue';
 export default {
   setup() {
-    const uName = ref('Maximilian');
+    // const uName = ref('Maximilian');
+    // const uAge = ref(31);
+
+    // const user = ref({
+    //   name: 'Maximilian',
+    //   age: 31,
+    // });
+
+    const user = reactive({
+      name: 'Maximilian',
+      age: 31,
+    });
 
     setTimeout(function () {
-      uName.value = 'Max';
+      // uName.value = 'Max';
+      // uAge.value = 32;
+
+      // user.value.name = 'Max'; - with refs
+      // user.value.age = 32;
+
+      user.name = 'Max'; //- with reactive
+      user.age = 32;
     }, 2000);
 
-    return { userName: uName };
+    // return { userName: uName, age: uAge };
+    return { user: user };
   },
   // data() {
   //   return {
