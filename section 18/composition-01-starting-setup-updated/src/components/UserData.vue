ca6bcd61aae5b2ec90f41be3220662443869e82a
pkindalov <panayotkindalov@gmail.com>
2023-01-17 13:02:36 +0200

    Emitting Custom Events

--- section 18/composition-01-starting-setup-updated/src/components/UserData.vue
+++ section 18/composition-01-starting-setup-updated/src/components/UserData.vue
@@ -1,17 +1,20 @@
 <template>
-  <h2>{{ userName }}</h2>
-  <h3>{{ age }}</h3>
+  <div>
+    <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+  </div>
 </template>
 
 <script>
 import { computed } from 'vue';
 export default {
   props: ['firstName', 'lastName', 'age'],
-  setup(props) {
+  setup(props, context) {
     const uName = computed(function () {
       return props.firstName + ' ' + props.lastName;
     });
-
+    // context.emit('save-data', 1); = this.$emit('save-data', 1);
+    console.log(context);
     return { userName: uName };
   },
   // computed: {
